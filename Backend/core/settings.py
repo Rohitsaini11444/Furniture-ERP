4aef413a9aee619b38387e4644101df20deb9cbb
Rohitsaini-11 <rohitsaini22444pce@gmail.com>
2026-07-16 18:56:50 +0530

    Allow Vercel frontend

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -94,6 +94,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://furniture-erp-sepia.vercel.app",
+
 ]
 CORS_ALLOW_CREDENTIALS = True
 
